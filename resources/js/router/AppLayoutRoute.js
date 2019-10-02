c38fde771cb505abbc8d71d8dd72e4820fd23c63
Ferdie De Oliveira <deferdie@gmail.com>
2019-10-02 17:04:54 +0100

    initial

---
+++ resources/js/router/AppLayoutRoute.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Route } from 'react-router-dom';
+import AppLayout from '../pages/layouts/AppLayout';
+import PropTypes from 'prop-types';
+
+const propTypes = {
+  component: PropTypes.func.isRequired,
+  rest: PropTypes.object
+};
+
+const AppLayoutRoute = ({ component: Component, ...rest }) => {
+  return (
+    <Route {...rest} render={props => (
+      <Component {...props} />
+    )} />
+  );
+};
+
+AppLayoutRoute.propTypes = propTypes;
+
+export default AppLayoutRoute;
---
+++ resources/js/router/AuthRoute.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import store from '../store';
+import { Route, Redirect } from 'react-router-dom';
+import AppLayoutRoute from './AppLayoutRoute';
+import { setIntendedUrl } from '../helpers/auth';
+import PropTypes from 'prop-types';
+import { ThemeProvider } from '@material-ui/styles';
+import theme from '../theme';
+import { Main as MainLayout, Minimal as MinimalLayout } from '../layouts';
+
+const propTypes = {
+  component: PropTypes.func.isRequired,
+  rest: PropTypes.object,
+  location: PropTypes.object
+};
+
+const AuthRoute = ({ component: Component, ...rest }) => (
+  <ThemeProvider theme={theme}>
+    <MainLayout>
+      <Route
+        {...rest}
+        render={props => {
+          const { auth: { authenticated } } = store.getState();
+
+          if (!authenticated) {
+            setIntendedUrl(props.location.pathname);
+          }
+
+          return authenticated ? (
+            <AppLayoutRoute component={Component} {...props} />
+          ) : (
+            <Redirect
+              to={{
+                pathname: '/signin',
+                state: { from: props.location }
+              }}
+            />
+          );
+        }
+        }
+      />
+    </MainLayout>
+  </ThemeProvider>
+);
+
+AuthRoute.propTypes = propTypes;
+AuthRoute.displayName = 'Auth Route';
+
+export default AuthRoute;
