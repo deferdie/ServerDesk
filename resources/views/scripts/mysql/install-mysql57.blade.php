590b89159788dcd83410ba2ae5215a9cdbe6753b
Ferdie De Oliveira <deferdie@gmail.com>
2019-10-05 23:59:35 +0100

    script to install mysql

---
+++ resources/views/scripts/mysql/install-mysql57.blade.php
@@ -0,0 +1,15 @@
+sudo apt update
+sudo apt install mysql-server -y
+sudo service mysql start
+sudo systemctl start mysql
+
+## Secure MYSQL
+sudo mysql --execute="SELECT user,authentication_string,plugin,host FROM mysql.user;"
+
+## Create a new database for serverdesk
+sudo mysql --execute="CREATE USER 'serverdesk'@'localhost' IDENTIFIED BY 'password';"
+sudo mysql --execute="GRANT ALL PRIVILEGES ON *.* TO 'serverdesk'@'localhost' WITH GRANT OPTION;"
+
+## Reset the root password
+sudo mysql --execute="ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'password';"
+sudo mysql --execute="FLUSH PRIVILEGES;" --password='password'
