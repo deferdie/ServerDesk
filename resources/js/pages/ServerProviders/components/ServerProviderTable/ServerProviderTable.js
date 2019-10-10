684a4976b25702499042cab808031dee1dd8a293
Ferdie De Oliveira <deferdie@gmail.com>
2019-10-10 22:55:00 +0100

    Workingon ability to remove provider

--- resources/js/pages/ServerProviders/components/ServerProviderTable/ServerProviderTable.js
+++ resources/js/pages/ServerProviders/components/ServerProviderTable/ServerProviderTable.js
@@ -1,10 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import clsx from 'clsx';
 import PropTypes from 'prop-types';
-import moment from 'moment';
 import PerfectScrollbar from 'react-perfect-scrollbar';
 import { makeStyles } from '@material-ui/styles';
 import {
+  Fab,
   Card,
   CardContent,
   Table,
@@ -15,6 +15,8 @@ import {
   Typography
 } from '@material-ui/core';
 import _ from 'lodash';
+import DeleteIcon from '@material-ui/icons/Delete';
+import SweetAlert from 'sweetalert-react';
 
 const useStyles = makeStyles(theme => ({
   root: {},
@@ -35,6 +37,11 @@ const useStyles = makeStyles(theme => ({
 const ServerProviderTable = props => {
   const { className, providers, ...rest } = props;
   const classes = useStyles();
+  const [providerToDelete, setProviderToDelete] = useState(false);
+
+  const deleteProvider = () => {
+    console.log('removeing provider');
+  };
 
   return (
     <Card
@@ -49,30 +56,51 @@ const ServerProviderTable = props => {
                 <TableRow>
                   <TableCell>Name</TableCell>
                   <TableCell>Server Provider</TableCell>
-                  <TableCell>Server created</TableCell>
+                  <TableCell>Actions</TableCell>
                 </TableRow>
               </TableHead>
               <TableBody>
-                {providers.map(provider => (
-                  <TableRow
-                    className={classes.tableRow}
-                    hover
-                    key={provider.id}
-                  >
-                    <TableCell>
-                      <Typography variant="body1">{_.get(provider, 'name')}</Typography>
-                    </TableCell>
-                    <TableCell>{_.get(provider, 'server_provider.name')}</TableCell>
-                    <TableCell>
-                      {moment(provider.created_at).format('DD/MM/YYYY')}
-                    </TableCell>
-                  </TableRow>
-                ))}
+                {providers.map((provider, index) => {
+                  return (
+                    <TableRow
+                      className={classes.tableRow}
+                      hover
+                      key={provider.id}
+                    >
+                      <TableCell>
+                        <Typography variant="body1">{_.get(provider, 'name')}</Typography>
+                      </TableCell>
+                      <TableCell>{_.get(provider, 'server_provider.name')}</TableCell>
+                      <TableCell>
+                        <Fab
+                          size="small"
+                          color="secondary"
+                          aria-label="edit"
+                          onClick={() => setProviderToDelete(index)}
+                        >
+                          <DeleteIcon />
+                        </Fab>
+                      </TableCell>
+                    </TableRow>
+                  )
+                })}
               </TableBody>
             </Table>
           </div>
         </PerfectScrollbar>
       </CardContent>
+      {/* Alert to delete a database */}
+      <SweetAlert
+        show={Boolean(providerToDelete !== false)}
+        showCancelButton
+        type="error"
+        title="Are you sure you want to delete this provider?"
+        text={`You are about to this provider ${_.get(providers, [providerToDelete, 'name'])}`}
+        onConfirm={deleteProvider}
+        onCancel={() => {
+          setProviderToDelete(false);
+        }}
+      />
     </Card>
   );
 };
