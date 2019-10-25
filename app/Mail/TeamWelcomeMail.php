74558595ceca07d25d9624c3a2e8c08942e666e0
Ferdie De Oliveira <deferdie@gmail.com>
2019-10-25 11:21:44 +0100

    Working on welcome email

---
+++ app/Mail/TeamWelcomeMail.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Mail;
+
+use App\User;
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class TeamWelcomeMail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * The user to welcome email
+     *
+     * @var \App\User
+     */
+    public $email;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct(User $user)
+    {
+        $this->user = $user;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->view('view.name');
+    }
+}
