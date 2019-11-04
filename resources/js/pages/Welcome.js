cfc6590f8337d38daabba3893ddae533822f05a1
Ferdie De Oliveira <deferdie@gmail.com>
2019-11-04 12:11:35 +0000

    Working on frontend marketing

--- resources/js/pages/Welcome.js
+++ resources/js/pages/Welcome.js
@@ -5,16 +5,19 @@ import GuestNav from '../components/GuestNav';
 const Welcome = () => {
   return (
     <DocumentTitle title={`Welcome - ${window.App.name}`}>
-      <div className="flex flex-col min-h-screen">
+      <div className="flex flex-col min-h-screen homepage-background">
+        <div className="automation-background"></div>
         <GuestNav />
 
-        <div className="flex flex-1 flex-col items-center justify-center">
-          <h1 className="text-indigo p-2"> Hello <span className="">Dev!</span> Start crafting your app now!</h1>
+        <div className="flex flex-1 flex-col items-left">
+          <h1 className="p-2 text-white homepage-heading">
+            Hello <span>Developer!</span> Are you ready to deploy your app!
+          </h1>
 
-          <div className="flex items-center">
-            <img src='/images/icons/laravel.svg' className="h-32" />
-            <span className="text-3xl font-bold">&#43;</span>
-            <img src='/images/icons/react.svg' className="h-24" />
+          <div>
+            <button type="button" className="p-3 text-black bg-white font-bold homepage-heading-signup-button">
+              GET STARTED
+            </button>
           </div>
         </div>
 
