f5012691811cba13617b7eadad4eeccc7a562bd0
Ferdie De Oliveira <deferdie@gmail.com>
2019-11-06 12:37:04 +0000

    Added ability to get api token from auth

--- tests/Feature/UpTimeCheckTest.php
+++ tests/Feature/UpTimeCheckTest.php
@@ -8,15 +8,21 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class UpTimeCheckTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     *
-     * @return void
-     */
+    use RefreshDatabase;
+    
     public function test_unauthenticated_users_cannot_get_uptimechecks()
     {
         $response = $this->get('/api/uptime');
 
         $response->assertStatus(401);
+    }
+    
+    public function test_authenticated_users_can_get_their_uptimechecks()
+    {
+        $response = $this->get('/api/uptime');
+
+        $this->signIn();
+
+        // $response->assertStatus(401);
     }
 }
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,22 @@
 
 namespace Tests;
 
+use App\User;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    public function signIn(User $user = null)
+    {
+        $user = $user ?? factory('App\User')->create();
+
+        $token = auth()->attempt([
+            'email' => $user->email,
+            'password' => 'secret',
+        ]);
+
+        return $token;
+    }
 }
